eed7302b43d0b3acf8d57ecc50818c01c96700be
Marius Schmid <mafloschmid@aol.com>
2024-04-05 11:52:49 +0200

    added comments

--- car-go-brrr.X/main.c
+++ car-go-brrr.X/main.c
@@ -1,17 +1,17 @@
 /*
  * File:   main.c
- * Author: Maflo
+ * Author: Marius
  *
  * Created on 1. April 2024, 17:12
  */
 // CONFIG
-#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
-#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
-#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config FOSC = INTRCIO // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = OFF     // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF    // Power-Up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF    // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
+#pragma config BOREN = OFF    // Brown-out Detect Enable bit (BOD disabled)
+#pragma config CP = OFF       // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF      // Data Code Protection bit (Data memory code protection is disabled)
 
 #define _XTAL_FREQ 4000000
 #include <xc.h>
@@ -19,30 +19,24 @@
 #define WAIT_UNTIL_TRIGGER_S 40
 #define ACTIVE_RELAIS_TIME_S 1
 
+void main(void)
+{
+    TRISIO = 0x00; // make all GPIOs as output
+    GP5 = 0; //set GPIO LOW
 
-void main(void) {
-    TRISIO=0x00;
-    GP5 = 0;
-#if 1
-    for(uint8_t i = 0; i < WAIT_UNTIL_TRIGGER_S; i++){
+    for (uint8_t i = 0; i < WAIT_UNTIL_TRIGGER_S; i++)
+    {
         __delay_ms(1000);
-    }  
-    GP5 = 1;
-    for(uint8_t i = 0; i < ACTIVE_RELAIS_TIME_S; i++){
+    }
+    GP5 = 1; //set GPIO HIGH
+    for (uint8_t i = 0; i < ACTIVE_RELAIS_TIME_S; i++)
+    {
         __delay_ms(1000);
-    }  
-    GP5 = 0;
-    while(1){
+    }
+    GP5 = 0; //set GPIO LOW
+    //Endless loop
+    while (1)
+    {
         __delay_ms(500);
     }
-#else
-    while(1){
-        GP5 = 1 ;
-        __delay_ms(1000);
-        GP5 = 0 ;
-        __delay_ms(1000);
-    }
-    return;  
-#endif
-
 }
